ae2ee50bbaa18bc53493013f07cc7f16fe7f7ce2
Suresh <suridl@Sureshs-MacBook-Air.local>
2017-02-10 22:00:14 +0530

    Initial Commit

---
+++ gesturesNew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gesturesNew
+//
+//  Created by Suresh on 2/10/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
