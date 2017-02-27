414e59093e5ae6d758701b63bb1f405391544018
dlsuresh <dl.suresh6@gmail.com>
2017-02-27 18:21:30 +0530

    initial commit

--- gesturesNew/ViewController.m
+++ gesturesNew/ViewController.m
@@ -9,21 +9,64 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+-(void)SwipeRecogniseLeft:(UISwipeGestureRecognizer *)gesture;
+-(void)SwipeRecogniseRight:(UISwipeGestureRecognizer *)gesture;
+
+@property (strong, nonatomic) IBOutlet UIView *view2;
+@property (strong, nonatomic) IBOutlet UIView *view3;
+
 
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    
+    UISwipeGestureRecognizer *Rightswipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(SwipeRecogniseRight:)];
+     Rightswipe.direction = UISwipeGestureRecognizerDirectionRight;
+    UISwipeGestureRecognizer *Leftswipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(SwipeRecogniseLeft:)];
+    Leftswipe.direction = UISwipeGestureRecognizerDirectionLeft;
+//    self.pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panGesture:)];
+
+    [self.view3 addGestureRecognizer:Rightswipe];
+    [self.view3 addGestureRecognizer:Leftswipe];
+   // [self.view2 addGestureRecognizer:self.pan];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+-(void)SwipeRecogniseLeft:(UISwipeGestureRecognizer *)gesture{
+    NSLog(@"swipe left");
+
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view3.frame = CGRectOffset(self.view3.frame, -110.0, 0.0);
+    }];
+    
+}
+-(void)SwipeRecogniseRight:(UISwipeGestureRecognizer *)gesture{
+    NSLog(@"swipe right");
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view3.frame = CGRectOffset(self.view3.frame, 110.0, 0.0);
+    }];
+    
+
+
+
 }
 
 
+- (IBAction)panGesture:(UIPanGestureRecognizer *)sender {
+
+        CGPoint translation = [sender  translationInView:self.view2];
+        sender.view.center = CGPointMake(sender.view.center.x + translation.x, sender.view.center.y + translation.y);
+        [sender setTranslation:CGPointZero inView:self.view2];
+    
+}
 @end
